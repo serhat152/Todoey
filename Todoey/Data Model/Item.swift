405fff1025ec84d112284b6a0f1ae2e2594ca65c
serhatyuksel <serhatyksel@gmail.com>
2018-08-13 20:13:10 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Serhat Yuksel on 13.08.2018.
+//  Copyright © 2018 Serhat Yuksel. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+}
